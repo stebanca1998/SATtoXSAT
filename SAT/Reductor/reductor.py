c36f8e66ae4ec5b24b2e007ba3b1a60a93f779dd
Jem Pool <jem.suarez@correounivalle.edu.co>
2020-09-27 11:50:49 -0500

    añadido el procesamiento de las clausulas para k<x

--- SAT/Reductor/reductor.py
+++ SAT/Reductor/reductor.py
@@ -1,5 +1,7 @@
 from os import listdir
 from os.path import isfile, join
+from itertools import product
+import numpy as np
 
 onlyfiles = [f for f in listdir("../InstanciasSAT/") if isfile(join("../InstanciasSAT/", f))]
 
@@ -69,6 +71,35 @@ def ListaDeClausulasAArchivo(var,claus,path):
 
 
 '''
+Función auxiliar que genera nuevas variables numéricas aún no usadas, recibe 1 argumento:
+    max_variable_usada: mayor valor numérico usado por una variable en el conjunto de entrada
+    (observación -> se incializa solo una vez en todo el programa)
+Retorna una nueva varible a partir de 'max_variable_usada + 1', cada que se llama con el metodo 'next(var)'
+'''
+def GeneradorDeVariables(max_variable_usada):
+
+    n = max_variable_usada + 1
+    while True:
+        yield n
+        n += 1
+
+'''
+Esta función recibe 3 argumentos:
+    clausula: la clausula actual de la lista de clausulas que van a ser convertidas a X-SAT    
+    x: numero que me informa a que tipo X-SAT van a ser convertidas las clausulas
+    nuevasClausulas: variable donde se almacenarán las clausulas generadas
+NO retorna nada, ya que adiciona el resultado directamente a las clausulas de salida en 'nuevasClausulas'
+'''
+def GenerarNuevasClausulas(clausula, x, nuevasClausulas):
+    
+    num_variables_adicionales = x-len(clausula)    
+    lista_variables_adicionales = [next(variable) for i in range(num_variables_adicionales)]    
+    matriz_de_signos = [list(i) for i in product([1,-1],repeat=num_variables_adicionales)]
+        
+    for var in matriz_de_signos:
+        nuevasClausulas.append( clausula + (np.array(lista_variables_adicionales) * np.array(var)).tolist() )
+
+'''
 Esta función recibe 3 objetos:
 	clausulas: Una lista de las clausulas pertenecientes a las instancias que van a ser convertidas a X-SAT
 	x: numero que me informa a que tipo X-SAT van a ser convertidas las clausulas
@@ -79,11 +110,16 @@ def ConvertirSATaXSAT(clausulas, x, var):
 
 	nuevasClausulas = list()
 	maxvar = int(var)
+
+	max_variable_usada = max( [item for sublist in clausulas for item in sublist] )
+	global variable
+	variable = GeneradorDeVariables(max_variable_usada) # inicializar el generador	
+	
 	
 	for clause in clausulas:
 
 		if( len(clause) < x ):
-			continue
+			GenerarNuevasClausulas(clause, x, nuevasClausulas)
 
 		if( len(clause) == x ):
 			nuevasClausulas.append(clause)
